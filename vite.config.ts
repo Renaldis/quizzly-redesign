75abf6185eee161ca8e8371e7498070c6362d9a9
renaldis <renaldis142@gmail.com>
2026-03-16 11:05:20 +0700

    fix manualchunk bundler

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,33 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/antd')) {
+            return 'antd';
+          }
+          if (id.includes('node_modules/@ant-design')) {
+            return 'ant-icons';
+          }
+          if (id.includes('node_modules/react-router')) {
+            return 'router';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
     },
   },
 });
